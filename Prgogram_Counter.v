70bc1f52428ca814cacbc913192ea9af60f36140
R Santhosh Singh <157267582+RajputhSanthoshSingh@users.noreply.github.com>
2025-01-16 23:24:47 +0530

    Update Prgogram_Counter.v

--- Prgogram_Counter.v
+++ Prgogram_Counter.v
@@ -1,13 +1,12 @@
 module Program_Counter (
-    input wire clk,rst, // Clock and Reset
-    ouput reg [31:0] PC // Program Counter
+    input wire clk, rst, // Clock and Reset
+    output reg [31:0] PC // Program Counter
     );
 
     always @(posedge clk or posedge rst) begin
-        if(rst) begin
+        if (rst) begin
             PC <= 32'b0;
-        end
-        else begin
+        end else begin
             PC <= PC + 32'b4;
         end
     end
